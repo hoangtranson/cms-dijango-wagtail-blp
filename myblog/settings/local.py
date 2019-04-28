948a236bb6b37a2994305a0cd027884578bc34d8
Tran Son Hoang <hayden.tran.d@gmail.com>
2019-04-28 13:03:50 +0800

    update local

--- myblog/settings/local.py
+++ myblog/settings/local.py
@@ -1,13 +1,13 @@
 SECRET_KEY = 'lpt!ja!7y&wl#y-u$en&2h11b1#5gaxz=^t#8z&&j16+g^5qv='
 ALLOWED_HOSTS = ['transonhoang.pythonanywhere.com']
 
-STATIC_ROOT = '/home/transonhoang/cms-dijango-wagtail-blp/mysite/static'
+STATIC_ROOT = '/home/transonhoang/cms-dijango-wagtail-blp/myblog/static'
 MEDIA_ROOT = '/home/transonhoang/cms-dijango-wagtail-blp/media'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # do not change it
-        'NAME': 'transonhoang$default',  # add database_name
+        'NAME': 'transonhoang$myblog',  # add database_name
         'USER' : 'transonhoang',  # add user_name
         'PASSWORD' : 'hoang123456789',  # add password_here
         'HOST' : 'transonhoang.mysql.pythonanywhere-services.com', # add host_name
